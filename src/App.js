cf62d4d04f7a9791ea40e238c4a44135496df3aa
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-10-10 20:52:28 -0300

    #FEAT: Adicionado controle de frase sem useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // lista de frases (estado)
+
+  // o usuário pode adicionar novas frases, desde que:
+    // a frase possua mais do que 20 caracteres
+    // a frase seja única
+
+  const [frase, setFrase] = useState("");
+  const [frases, setFrases] = useState([]);
+
+  const salvarFrase = (evento) => {
+    evento.preventDefault();
+
+    if (frase.length < 20) {
+      alert("Ops... A frase precisa ter, pelo menos, 20 caracteres!");
+      return;
+    }
+
+    if (frases.includes(frase)) {
+      alert("Ops... Não pode haver frases duplicadas!");
+      return;
+    }
+
+    setFrases([...frases, frase]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={salvarFrase}>
+        <textarea value={frase} onInput={(evento) => setFrase(evento.target.value)} required/>
+        <br/>
+        <button>Salvar</button>
+      </form>
+
+      {frases.map((fraseAtual) => <p>{fraseAtual}</p>)}
     </div>
   );
 }
